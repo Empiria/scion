c3b16dbc9eef14ddbf97952b374904aa1d20777b
Preston Holmes <ptone@google.com>
2026-01-10 15:57:47 -0800

    fix up some build artifact stuff

--- hack/trigger-cloudbuild.sh
+++ hack/trigger-cloudbuild.sh
@@ -3,5 +3,6 @@ set -e
 
 echo "Submitting build to Cloud Build..."
 gcloud builds submit --async \
+--project=ptone-misc \
 --substitutions=SHORT_SHA=$(git rev-parse --short HEAD) \
 --config image-build/cloudbuild.yaml .
